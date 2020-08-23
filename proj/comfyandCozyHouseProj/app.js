e54a80d8286f654c96b2733764ed1cb1a4e41125
Sundara Ram Matta <ram@Sundaras-MacBook-Pro.local>
2020-08-22 20:37:02 -0400

    adding classes

--- proj/comfyandCozyHouseProj/app.js
+++ proj/comfyandCozyHouseProj/app.js
@@ -8,3 +8,22 @@ const cartOverlay = document.querySelector('.cart-overlay');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productDOM = document.querySelector('.products-center');
+// cart
+let cart = [];
+// getting the products
+class Products {
+
+}
+// display the products
+class UI {
+
+}
+// local storage
+class Storage {
+
+}
+// Event Listener
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+})
