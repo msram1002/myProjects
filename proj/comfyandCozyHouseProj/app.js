35672d39fed4da9f95c1a8bae2203bd510b06925
Sundara Ram Matta <ram@Sundaras-MacBook-Pro.local>
2020-08-22 20:35:34 -0400

    declaring initial variables

---
+++ proj/comfyandCozyHouseProj/app.js
@@ -0,0 +1,10 @@
+// declaring variables
+const cartBtn = document.querySelector('.cart-btn');
+const cartItems = document.querySelector('.cart-items');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productDOM = document.querySelector('.products-center');
