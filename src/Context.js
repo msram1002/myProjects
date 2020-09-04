37ba116531dc54e5c198bf0bc693654c6f855672
Sundara Ram Matta <ram@Sundaras-MacBook-Pro.local>
2020-09-03 23:56:57 -0400

    adding featured rooms

--- src/Context.js
+++ src/Context.js
@@ -11,12 +11,18 @@ class RoomProvider extends Component {
     };
     // getData
     // As the name suggests, after all the elements of the page is rendered correctly, 
-    //this method is called. 
+    // this method is called. 
     // After the markup is set on the page, this technique called by React itself to either 
     // fetch the data from An External API or perform some unique operations which need the JSX elements.
     componentDidMount(){
         let rooms = this.formatData(items);
-        console.log(rooms);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        this.setState({
+            rooms, 
+            featuredRooms, 
+            sortedRooms:rooms, 
+            loading: false
+        })
     }
     // flattening the data from data.js
     formatData(items){
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react'
 import {RoomContext} from '../Context';
+import Loading from './Loading';
+import Title from './Title';
+import Room from './Room';
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
     render() {
-        const {greeting, name} = this.context;
+        let {loading, featuredRooms: featRooms} = 
+        this.context;
+        featRooms = featRooms.map(featroom => {
+            return <Room key={featroom.id}
+            room={featroom} />
+        })
         return (
-            <div>
-                {greeting} from {name}
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div className="featured-rooms-center">
+                    {loading?<Loading />: featRooms}
+                </div>
+            </section>
         )
     }
 }
