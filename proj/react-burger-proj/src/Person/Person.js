c782c317e14f8c2c577bb79c1ef30140cbc31809
Sundara Ram Matta <msram0210@gmail.com>
2021-02-17 16:52:57 -0500

    css changes

--- proj/react-burger-proj/src/Person/Person.js
+++ proj/react-burger-proj/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
-      <p>I am {props.name} and age is {props.age}.</p>
+    <div className="person">
+      <h2>I am {props.name} and age is {props.age}.</h2>
       <p>{props.children}</p>
     </div>
   );
