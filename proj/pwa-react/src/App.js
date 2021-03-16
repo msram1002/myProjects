b92e8d3e399bb321f938cbfc406b6d4574cc52f8
Sundara Ram Matta <msram0210@gmail.com>
2021-03-15 21:30:56 -0400

    progressive web app

---
+++ proj/pwa-react/src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {fetchWeather} from './api/fetchWeather';
+import './App.css';
+
+const App = () => {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
+  
+  const search = async (e) => {
+    if (e.key === 'Enter') {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      // Resetting the query to blank, clearing the input
+      setQuery('');
+    }
+  }
+
+  return (
+    <div className="main-container">
+      <input type="text" 
+        className="search"
+        placeholder="Search for a city ..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search} />
+    </div>
+  );
+}
+
+export default App;
+
