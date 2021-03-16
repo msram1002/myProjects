5c4c39caece6b0664db58eeb1a055288301c67be
Sundara Ram Matta <msram0210@gmail.com>
2021-03-15 21:43:43 -0400

    creating the react UI and fetching the weather

--- proj/pwa-react/src/App.js
+++ proj/pwa-react/src/App.js
@@ -24,6 +24,23 @@ const App = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search} />
+      {/* if weatehr amin exists */}
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;F</sup>
+          </div>
+          <div className="info">
+            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}  
     </div>
   );
 }
