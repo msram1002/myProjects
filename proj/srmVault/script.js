496fd152c2fb208b14af7d0a95fc4d7900f708f5
Sundara Ram Matta <msram0210@gmail.com>
2021-08-06 10:48:04 -0400

    removing unnecessary code

--- proj/srmVault/script.js
+++ proj/srmVault/script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// SRM VAULT APP
 
 // Data
 const account1 = {
@@ -62,15 +62,4 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
